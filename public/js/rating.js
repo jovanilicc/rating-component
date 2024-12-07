5439e47199d4b54416d568bb3f6f071a0dcd8eac
Jovan Ilic <jovanim89.ilic@gmail.com>
2024-12-07 19:47:44 +0100

    Added form submit functionality

--- public/js/rating.js
+++ public/js/rating.js
@@ -1,6 +1,9 @@
 let rateItem = document.querySelectorAll(".rating-list__item");
 let inputItem = document.getElementById("rate");
 let form = document.getElementById("rate-form");
+let rateText = document.getElementsByClassName("rate-text");
+let ratingBox = document.getElementById("rating-box");
+let thankyouBox = document.getElementById("thankyou-box");
 
 let selectedListElement = null;
 let rateValue = "";
@@ -22,5 +25,20 @@ const selectRate = (e) => {
 rateItem.forEach((element) => {
   element.addEventListener("click", selectRate);
 });
+
+const updateRate = (e) => {
+  e.preventDefault();
+  if (selectedListElement) {
+    rate.setAttribute("value", rateValue);
+    ratingBox.style.display = "none";
+    thankyouBox.style.display = "block";
+    rateText[0].textContent = rateValue;
+    setTimeout(() => {
+      form.submit();
+    }, 4000);
+  } else {
+    window.alert("Please select your rating.");
+  }
+};
 
 form.addEventListener("submit", updateRate);
